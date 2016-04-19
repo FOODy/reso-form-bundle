63adce3d692c008c9b740af1f971a6c14bb518c8
Vitalij Rerich <info@rerich-software.de>
2016-04-19 14:26:18 +0200

    Update.

--- src/Reso/Bundle/FormBundle/ResoFormBundle.php
+++ src/Reso/Bundle/FormBundle/ResoFormBundle.php
@@ -2,9 +2,17 @@
 
 namespace Reso\Bundle\FormBundle;
 
+use Reso\Bundle\FormBundle\DependencyInjection\Compiler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ResoFormBundle extends Bundle
 {
-
+	/**
+	 * @param ContainerBuilder $container
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		$container->addCompilerPass(new Compiler\EventSubscriberPass());
+	}
 }
